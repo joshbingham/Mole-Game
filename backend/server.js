d758263c89a164f8acdd0fe9937bb0bec25311e4
Josh Bingham <joshuabingham@hotmail.co.uk>
2026-03-09 13:39:10 +0000

    Create POST endpoint for storing scores

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const scores = [];
+
 app.get("/", (req, res) => {
   res.send("Mole Game API running");
+});
+
+app.post("/scores", (req, res) => {
+
+  const { name, score } = req.body;
+
+  const newScore = {
+    name,
+    score,
+    date: new Date()
+  };
+
+  scores.push(newScore);
+
+  console.log("New score added:", newScore);
+
+  res.json({ message: "Score saved successfully" });
+
 });
 
 app.listen(3000, () => {
