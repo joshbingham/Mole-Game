886159dc790f68c03922f7e6161c15e13478e63f
Josh Bingham <joshuabingham@hotmail.co.uk>
2026-03-11 17:58:10 +0000

    Scale game to browser window

--- frontend/game.js
+++ frontend/game.js
@@ -1,12 +1,19 @@
 const config = {
-	type: Phaser.AUTO,
-	width: 480,
-	height: 640,
-	physics: {
-		default: 'arcade',
-	},
-	backgroundColor: '#F8B392',
-	scene: [MenuScene, StartScene, GameScene, EndScene, LeaderboardScene],
+  type: Phaser.AUTO,
+  width: 480,
+  height: 640,
+  backgroundColor: '#2d1b0f',
+
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+
+  physics: {
+    default: 'arcade'
+  },
+
+  scene: [MenuScene, StartScene, GameScene, EndScene, LeaderboardScene]
 };
 
 const game = new Phaser.Game(config);
